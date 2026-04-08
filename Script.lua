a2cbba556e78677b311ebdd611f236ed827231b0
hubak8388-art <hubak8388@gmail.com>
2026-04-08 09:13:58 +0300

    Script.lua

---
+++ Script.lua
@@ -0,0 +1,13 @@
+print("---------------------------------")
+print("Lucky Block Script: СТАТУС - АКТИВЕН")
+print("Автор: hubak8388-art")
+print("---------------------------------")
+
+-- Пример простой функции: уведомление в игре
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Lucky Block Mod",
+    Text = "Скрипт успешно загружен из GitHub!",
+    Duration = 5
+})
+
+-- Здесь в будущем будет логика поиска блоков
